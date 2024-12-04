76e9a7db64a1ec9918a9eafa695d1a0536c17182
Jason Wall <jason.wall@vaeit.com>
2024-12-04 09:10:08 -0500

    Add back the parser cli project

---
+++ src/JMW.Parser/Options.cs
@@ -0,0 +1,10 @@
+using CommandLine;
+
+public class Options
+{
+    [Option("json", Required = false)]
+    public bool UseJson { get; set; }
+
+    [Option("ifconfig", Required = false)]
+    public bool UseIfconfig { get; set; }
+}
