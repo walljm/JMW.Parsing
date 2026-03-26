647f4bfc5c8b70b3bb3d541673bc11892b22114d
Jason Wall <jason.wall@vaeit.com>
2026-03-26 10:24:55 -0400

    Refactor build script to include publishing for linux-arm64 and linux-x64 targets

--- src/JMW.MachineInfo/build.sh
+++ src/JMW.MachineInfo/build.sh
@@ -1,3 +1,9 @@
 dotnet publish -r osx-arm64 -p:PublishSingleFile=true --self-contained true
 cp -f ./bin/Release/net10.0/osx-arm64/publish/jmwmachineinfo ~/projects/walljm/personal/profiles/mac/utils
- 
+
+dotnet publish -r linux-arm64 -p:PublishSingleFile=true --self-contained true
+cp -f ./bin/Release/net10.0/linux-arm64/publish/jmwmachineinfo ~/projects/walljm/personal/profiles/bash/utils/jmwmachineinfo-linux-arm64
+
+dotnet publish -r linux-x64 -p:PublishSingleFile=true --self-contained true
+cp -f ./bin/Release/net10.0/linux-x64/publish/jmwmachineinfo ~/projects/walljm/personal/profiles/bash/utils/jmwmachineinfo-linux-x64
+
